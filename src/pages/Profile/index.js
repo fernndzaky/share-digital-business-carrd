e8364f92e5aae4c68154c5ea0cbf51126e283fb4
fernndzaky <fernandhadzaky@hotmail.com>
2023-05-29 21:24:41 +0100

    next try

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -151,30 +151,23 @@ class Profile extends React.Component {
   
   handleContactClick = async (e) => {
 
-    const contact = {
+    // Get the contact information from the website
+    var contact = {
       name: "John Smith",
       phone: "555-555-5555",
-      email: "john@example.com",
+      email: "john@example.com"
     };
-
-    const vcard = `BEGIN:VCARD
-    VERSION:4.0
-    FN:${contact.name}
-    TEL;TYPE=work,voice:${contact.phone}
-    EMAIL:${contact.email}
-    END:VCARD`;
+    // create a vcard file
+    var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nTEL;TYPE=work,voice:" + contact.phone + "\nEMAIL:" + contact.email + "\nEND:VCARD";
+    var blob = new Blob([vcard], { type: "text/vcard" });
+    var url = URL.createObjectURL(blob);
     
-      const blob = new Blob([vcard], { type: "text/vcard" });
-      const url = URL.createObjectURL(blob);
+    const newLink = document.createElement('a');
+    newLink.download = contact.name + ".vcf";
+    newLink.textContent = contact.name;
+    newLink.href = url;
     
-      const newLink = document.createElement("a");
-      newLink.download = `${contact.name}.vcf`;
-      newLink.textContent = contact.name;
-      newLink.href = url;
-    
-      document.body.appendChild(newLink);
-      newLink.click();
-      document.body.removeChild(newLink);
+    newLink.click();
    
   }
 
