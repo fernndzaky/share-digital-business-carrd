65e92ab4199aa850ae14d2bdfb6f029ae770dd8a
fernndzaky <fernandhadzaky@hotmail.com>
2023-06-07 12:32:19 +0100

    repair chat

--- src/components/Clients/WhiteButton.js
+++ src/components/Clients/WhiteButton.js
@@ -8,7 +8,7 @@ import '../../assets/css/Clients/components.css';
 export default class WhiteButton extends Component {
   render(){
       return(
-        <a href="https://api.whatsapp.com/send?phone=+447498958903&text=Hallo%21%20Bizz%20" target="_blank" rel="noopener noreferrer" className="white-button font-size-18 lato-bold">
+        <a href="https://api.whatsapp.com/send?phone=+447498958903&text=Hallo%20Bizz%21%20" target="_blank" rel="noopener noreferrer" className="white-button font-size-18 lato-bold">
             {this.props.buttonText}
         </a>
         )
